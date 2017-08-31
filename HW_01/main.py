97b7cfbb8a4c8c266357790836a3f6f3cad2a22d
Vishveswaran Jothi <vjothi@purdue.edu>
2017-08-31 15:17:24 -0400

    Add files via upload

--- HW_01/main.py
+++ HW_01/main.py
@@ -24,33 +24,33 @@ image[2,:,:]=C3
 o_channel=3
 print(image.shape)
 # Part-A
-#conv2D=Conv2D(3,o_channel,3,2,'known')
-#no_ops,output_temp=conv2D.forward(image)
-#output=output_temp.numpy()
-#print (no_ops)
-#for i in range(o_channel):
-#    cv2.imwrite("Task3_img2_channel_"+str(i)+".jpg",output[i,:,:])
+conv2D=Conv2D(3,o_channel,3,2,'known')
+no_ops,output_temp=conv2D.forward(image)
+output=output_temp.numpy()
+print (no_ops)
+for i in range(o_channel):
+    cv2.imwrite("Task3_img1_channel_"+str(i)+".jpg",output[i,:,:])
 
-## Part- B
-#L_channels= np.asarray([2**i for i in range(5)])
-#L_time=np.zeros((5,1),dtype='float32')
-#for i in range(len(L_channels)):
-#    #L_channels[i]=2**i
-#    o_channel=L_channels[i]
-#    conv2D=Conv2D(3,o_channel,3,1,'rand')
-#    start_time = timeit.default_timer()
-#    no_ops,output_temp=conv2D.forward(image)
-#    L_time[i]=timeit.default_timer() - start_time
-#    print(timeit.default_timer() - start_time)
-##    output=output_temp.numpy()
-##    cv2.imwrite("Dummy"+str(i)+".jpg",output[i,:,:])
-#print(L_time)
-#fig = plt.figure()
-#a=plt.subplot(111)
-#a.plot(L_channels,L_time)
-#plt.title('Channels Vs Time')
-#fig.savefig('./Figure_PartB.png')
-#plt.close(fig)
+# Part- B
+L_channels= np.asarray([2**i for i in range(5)])
+L_time=np.zeros((5,1),dtype='float32')
+for i in range(len(L_channels)):
+    #L_channels[i]=2**i
+    o_channel=L_channels[i]
+    conv2D=Conv2D(3,o_channel,3,1,'rand')
+    start_time = timeit.default_timer()
+    no_ops,output_temp=conv2D.forward(image)
+    L_time[i]=timeit.default_timer() - start_time
+    print(timeit.default_timer() - start_time)
+#    output=output_temp.numpy()
+#    cv2.imwrite("Dummy"+str(i)+".jpg",output[i,:,:])
+print(L_time)
+fig = plt.figure()
+a=plt.subplot(111)
+a.plot(L_channels,L_time)
+plt.title('Channels Vs Time')
+fig.savefig('./Figure_PartB.png')
+plt.close(fig)
 
 #  Part- C
 kernel_size=np.asarray([3,5,7,9,11])
